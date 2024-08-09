46c6f6a9a9276164c5187b4c671a4978eb6f0209
kauana-sants <124754922+kauana-sants@users.noreply.github.com>
2024-08-09 15:34:33 -0300

    código

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import Todo from './components/Todo';
 import './App.css';
 import TodoForm from './components/TodoForm';
+import Search from './components/Search';
+import Filter from './components/Filter';
 
 function App() {
   const [todos, setTodos] = useState([
@@ -27,6 +29,11 @@ function App() {
 
   ]);
 
+  const [search, setSearch] = useState("")
+
+  const [filter, setFilter]  = useState('All')
+  const [sort, setSort] = useState('Asc')
+
   const addTodo = (text, category) => {
     const newTodos = [...todos, 
       {
@@ -40,13 +47,45 @@ function App() {
     setTodos(newTodos);
   }
 
+  const removeTodo = (id) => {
+    const newTodos = [...todos]
+    const filteredTodos = newTodos.filter(todo => todo.id !== id ? todo : null); // confere qual id foi enviado na função e excluí
+    setTodos(filteredTodos);
+  }
+
+  const completeTodo = (id) => {
+    const newTodos = [...todos]
+    newTodos.map((todo) => todo.id === id ? todo.isCompleted = !todo.isCompleted : todo);
+    setTodos(newTodos)
+  };
+
+
   return (
     <div className='app'>
       <h1>Lista de Tarefas</h1>
+      <Search search={search} setSearch={setSearch}/>
+      <Filter filter={filter} setFilter={setFilter}/>
       <div className="todo-list">
-        {todos.map((todo) =>(
-          <Todo key={todo.id} todo={todo}/>
-        ))}
+        {todos
+          .filter((todo) => 
+            filter === "All" 
+            ? true 
+            : filter === "Completed" 
+            ? todo.isCompleted 
+            : !todo.isCompleted
+            // Se estiver All não muda nada, 
+            // se estiver completed retorna apneas as que tiverem is completed como true
+            // Senão retorna todas as incompletas
+          )
+
+          .filter((todo) => 
+          todo.text.toLowerCase().includes(search.toLowerCase())
+          )
+
+          .map((todo) =>(
+          <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo}/>)
+          )
+        }
       </div>
       <TodoForm addTodo={addTodo}/>
     </div>
